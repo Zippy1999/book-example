293a71d171c1e56d4e8ccc338cd6f872c3d208a9
Richard Green <zippyrickgreen@yahoo.co.uk>
2017-01-14 07:39:02 +0000

    Basic view now returns minimal HTML

--- dtdd1/urls.py
+++ dtdd1/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from djangotdd import views as app_views
+
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', app_views.home_page, name='home'),
 ]
