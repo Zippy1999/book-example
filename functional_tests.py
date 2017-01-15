7dd2c89d2da7feb3ce61667e32eb08a5242bbd1a
Richard Green <zippyrickgreen@yahoo.co.uk>
2017-01-15 13:08:50 +0000

    Redirect after post and  show all items in template

--- functional_tests.py
+++ functional_tests.py
@@ -46,7 +46,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
-
+        time.sleep(10)
         # The page updates again, and now shows both items on her list
         self.check_for_row_in_list_table("1: Buy peacock feathers")
         self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
