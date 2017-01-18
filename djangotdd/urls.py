10d4b1ce41f8be83c8fd3fcf6c04e018827744e7
Richard Green <zippyrickgreen@yahoo.co.uk>
2017-01-18 19:51:17 +0000

    URL changes

--- djangotdd/urls.py
+++ djangotdd/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
 from djangotdd import views as app_views
 
@@ -21,6 +21,8 @@ from djangotdd import views as app_views
 
 urlpatterns = [
     url(r'^$', app_views.home_page, name='home'),
-    url(r'^lists/', include('djangotdd.urls')),
+    url(r'^(\d+)/$', app_views.view_list, name='view_list'),
+    url(r'^(\d+)/add_item$', app_views.add_item, name='view_list'),
+    url(r'^new$', app_views.new_list, name='new_list'),
     # url(r'^admin/', admin.site.urls)
 ]
